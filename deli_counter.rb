19824f65f8753e86384ba65cce70e0a5ba981706
Ernie Val <erndval@gmail.com>
2019-03-16 02:14:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,9 @@ def line(current_line)
   if current_line.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is "
+    puts "The line is currently:  "
+end
+
+def take_a_number(line, name)
+  line
 end
