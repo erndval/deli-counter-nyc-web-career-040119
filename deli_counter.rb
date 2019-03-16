7db82ce0951f2832d3d1f6e804057ea49db88282
Ernie Val <erndval@gmail.com>
2019-03-16 02:23:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ end
 
 def take_a_number(line, name)
   line.push(name)
-  puts "Welcome #{name}. You are number #{line.size+1} in line."
+  puts "Welcome #{name}. You are number #{line.size} in line."
 end
 
 def now_serving(line)
